e4eb8a39cc4cf484003cf69ae064a751f0185323
Jonathan Newman <j24newman@gmail.com>
2017-05-09 13:00:43 -0700

    Remove assets from symlinks defaults

--- attributes/default.rb
+++ attributes/default.rb
@@ -13,14 +13,13 @@ default['nginx']['source']['modules'] = %w(
 default['defaults']['global']['environment'] = 'production'
 default['defaults']['global']['symlinks'] = {
   'system' => 'public/system',
-  'assets' => 'public/assets',
   'cache' => 'tmp/cache',
   'pids' => 'tmp/pids',
   'log' => 'log'
 }
 default['defaults']['global']['create_dirs_before_symlink'] =
   %w(tmp public config ../../shared/cache ../../shared/assets)
-default['defaults']['global']['purge_before_symlink'] = %w(log tmp/cache tmp/pids public/system public/assets)
+default['defaults']['global']['purge_before_symlink'] = %w(log tmp/cache tmp/pids public/system)
 default['defaults']['global']['rollback_on_error'] = true
 
 # database
